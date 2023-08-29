8e0d9e5308abb5e2a2177c98c395bdcbd7ba2a0c
Anblicks <manoj.kumar@anblicks.com>
2023-08-29 14:12:13 +0000

    code2

--- models/Aggregate/SALES_SUMMARY.sql
+++ models/Aggregate/SALES_SUMMARY.sql
@@ -1,7 +1,6 @@
 {{
   config
   ( 
-    tags=["SALE"],
     materialized='table'    
   )
 }}
@@ -10,7 +9,7 @@ SELECT
     PRODUCT_GROUP
     ,SUM(STOCK_QTY) AS TOTAL_QTY
     ,SUM(COST) AS TOTAL_COST 
-FROM  ADQT_METADATA.PUBLIC.SALES_DATA 
+FROM  ADQT_METADATA.PUBLIC.{{ var('Input_Table') }} 
 WHERE 0=(SELECT total_records_failed FROM {{ ref('VALIDATE_WITH_CHECKPOINT_TABLE') }})
 GROUP BY 
     1
